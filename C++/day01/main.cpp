b78c965c42fda76ff36c9e0706ceaa3a947ad79c
Jade <862226994@qq.com>
2021-04-07 21:39:03 +0800

    两数相加

--- C++/day01/main.cpp
+++ C++/day01/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a = 0 , b = 0;
+    std::cout << "请输入两个数字" << std::endl;
+    std::cin >> a >> b;
+
+    std::cout << "a = "<< a << " b = " << b << " a + b = " << a +b << std::endl;
     return 0;
 }
