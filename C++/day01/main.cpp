882a3d60efb7aca1d06b837eb7ffbee9dfee1918
Jade <862226994@qq.com>
2021-04-08 20:36:58 +0800

    两数相乘

--- C++/day01/main.cpp
+++ C++/day01/main.cpp
@@ -2,10 +2,13 @@
 
 
 int main() {
+    std::cout << "Hello, World!" << std::endl;
     int a = 0 , b = 0;
     std::cout << "请输入两个数字" << std::endl;
     std::cin >> a >> b;
 
     std::cout << "a = "<< a << " b = " << b << " a + b = " << a +b << std::endl;
+
+    std::cout << "a * b =" << a * b << std::endl;
     return 0;
 }
